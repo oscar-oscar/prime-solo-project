5a3040f668298726a7150d499a96acf0ceca6855
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-10-19 01:37:29 -0500

    returing data via JSON.stringfy. GET working

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 //this is the main page of the app
 //user should see a list of their games played
 
 function Dashboard() {
 
+let [gameList, setGameList] = useState([]);
 
-    
+    const getGames = () => {
+        axios.get('/dashboard').then(response => {
+          console.log(response.data);
+          setGameList(response.data);
+        }).catch(e => {
+          console.log(e);
+          alert('something went wrong in axios GET getGames');
+        })
+      }
+
+      useEffect(() => {
+        getGames();
+      }, []);
+
+
     return <div>
         <ul>
-            <li>
-                <p>Match list here</p>
-            </li>
+            <li>{JSON.stringify(gameList)}</li>
+        
         </ul>
+            
+                {/* {
+                    gameList.map(game => {
+                        <li key = {game.id} />
+                    })
+                } */}
+                
+            
+        
     </div>;
   }
 

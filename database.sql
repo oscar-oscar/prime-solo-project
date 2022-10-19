2a67836a0c8ff6731f8c2fb44716d49692b16b1c
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-10-18 23:44:34 -0500

    corrected tables and sample data

--- database.sql
+++ database.sql
@@ -32,5 +32,57 @@ CREATE TABLE "match" (
 	"game_id" INT NOT NULL REFERENCES "game"
 
 );
+
+INSERT INTO "match"("id","team_1_score","team_2_score","game_id")
+VALUES (1,11,9,1);
+
+INSERT INTO "match"("id","team_1_score","team_2_score","game_id")
+VALUES (2,11,5,2);
+
+INSERT INTO "match"("id","team_1_score","team_2_score","game_id")
+VALUES (3,7,11,3);
+
+INSERT INTO "match"("id","team_1_score","team_2_score","game_id")
+VALUES (4,8,11,4);
+
+INSERT INTO "match"("id","team_1_score","team_2_score","game_id")
+VALUES (5,11,8,5);
+
+INSERT INTO "match"("id","team_1_score","team_2_score","game_id")
+VALUES (6,9,11,6);
+
 	
+INSERT INTO "game" (
+"id","user_id","played_at","location","notes","partner","opponent_1","opponent_2")
+VALUES 
+(1,1,'2022-10-08','Maple Grove', 'Game notes...', 'Billy', 'Exene', 'John');
+
+INSERT INTO "game" (
+"id","user_id","played_at","location","notes","partner","opponent_1","opponent_2")
+VALUES 
+(2,1,'2022-10-08','Maple Grove', 'Game Two', 'Billy', 'Exene', 'John');
+
+INSERT INTO "game" (
+"id","user_id","played_at","location","notes","partner","opponent_1","opponent_2")
+VALUES 
+(3,1,'2022-10-08','Maple Grove', 'Game three...', 'Billy', 'Exene', 'John');
+
+INSERT INTO "game" (
+"id","user_id","played_at","location","notes","opponent_1")
+VALUES 
+(4,1,'2022-10-08','Maple Grove', 'Game notes...', 'Exene');
+
+INSERT INTO "game" (
+"id","user_id","played_at","location","notes","opponent_1")
+VALUES 
+(5,1,'2022-10-08','Maple Grove', 'singles game 2', 'Exene');
+
+INSERT INTO "game" (
+"id","user_id","played_at","location","notes","opponent_1")
+VALUES 
+(6,1,'2022-10-08','Maple Grove', 'singles game 3', 'Exene');
+
+
+
+
 	

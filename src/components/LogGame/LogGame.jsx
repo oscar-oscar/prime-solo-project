5eff2ce8af614e39a9ec446a4edd0cb54171bec9
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-10-22 14:37:33 -0500

    create AddGame comp

---
+++ src/components/LogGame/LogGame.jsx
@@ -0,0 +1,18 @@
+//user log game form
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+
+function AddGame(){
+    console.log('in AddGame');
+
+    return(
+        <form>
+            <h2>Form Here</h2>
+        </form>
+    )
+}
+
+export default AddGame;

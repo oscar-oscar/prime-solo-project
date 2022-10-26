3974e9477f41834ef8ccd412020cc08b9402d8fb
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-10-26 03:43:46 -0500

    MUI form

--- src/components/AddGame/AddGame.jsx
+++ src/components/AddGame/AddGame.jsx
@@ -6,6 +6,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import './AddGame.css';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
 
 
 function AddGame() {
@@ -32,8 +34,8 @@ function AddGame() {
             payload: {
                 date: gameDate,
                 partner: partnerName,
-                opponent_1 : opponentOneName,
-                opponent_2 : opponentTwoName,
+                opponent_1: opponentOneName,
+                opponent_2: opponentTwoName,
                 score_a: myScore,
                 score_b: oppScore,
                 location: courtlocation,
@@ -45,99 +47,103 @@ function AddGame() {
     }
 
     return (
-        <div className="game-form">
-            <form onSubmit={sumbitForm}>
-                
-                <div class="AddGameH2">
-                <h2>Add Game Info</h2>
-                </div>
+        <Container>
 
-                <label>Date of Game</label>
-                <input
-                    type="date"
-                    required
-                    value={gameDate}
-                    onChange={(event) => setGameDate(event.target.value)}
-                />
-                <br/>
+            <div className="game-form">
+                <form onSubmit={sumbitForm}>
 
-                {/* <label>Match Type</label>
+                    <div class="AddGameH2">
+                        <h2>Add Game Info</h2>
+                    </div>
+
+                   
+                    <TextField sx={{ width: '25ch', margin: 3, marginTop: 1 }}
+                        type="date"
+                        required
+                        value={gameDate}
+                        onChange={(event) => setGameDate(event.target.value)}
+                    />
+                    <br />
+
+                    {/* <label>Match Type</label>
                 <select>
                     <option value="singles">Singles</option>
                     <option value="doubles">Doubles</option>
                 </select> */}
 
-                <label>Partner</label>
-                <input
-                    type="text"
-                    placeholder="Enter My Partner's Name"
-                    name="myScore"
-                    value={partnerName}
-                    onChange={(event) => setPartnerName(event.target.value)}
+                    
+                    <TextField sx={{ width: '25ch', margin: 3, marginTop: 1 }}
+                        label="Partner's Name"
+                        type="text"
+                        placeholder="Enter My Partner's Name"
+                        name="myScore"
+                        value={partnerName}
+                        onChange={(event) => setPartnerName(event.target.value)}
+                    />
+                    <br />
+                    <TextField sx={{ width: '25ch', margin: 3, marginTop: 1 }}
+                        label="Opponent One"
+                        type="text"
+                        required
+                        placeholder="Enter Oppoenent's Name"
+                        name="oppenent"
+                        value={opponentOneName}
+                        onChange={(event) => setOpponentOneName(event.target.value)}
+                    />
+                    <br />
+                    <TextField sx={{ width: '25ch', margin: 3, marginTop: 1 }}
+                        label="Opponent Two"
+                        type="text"
+                        placeholder="Enter Second Oppoenent's Name"
+                        name="oppenent-2"
+                        value={opponentTwoName}
+                        onChange={(event) => setOpponentTwoName(event.target.value)}
+                    />
+                    <br />
 
-                />
-                <br/>
+                    <TextField sx={{ width: '25ch', margin: 3, marginTop: 1 }}
+                        label="My Score"
+                        type="number"
+                        required
+                        placeholder="My Score"
+                        name="myScore"
+                        value={myScore}
+                        onChange={(event) => {
+                            const limit = 2;
+                            setMyscore(event.target.value.slice(0, limit));
+                        }}
+                    />
+                    
+                    <TextField sx={{ width: '25ch', margin: 3, marginTop: 1 }}
+                        label="Opponent Score"
+                        type="number"
+                        required
+                        placeholder="Opponents Score"
+                        name="oppScore"
+                        value={oppScore}
+                        onChange={(event) => {
+                            const limit = 2;
+                            setOppScore(event.target.value.slice(0, limit));
+                        }}
+                    />
+                    <br />
 
-                <label>Opponent's Name</label>
-                <input
-                    type="text"
-                    required
-                    placeholder="Enter Oppoenent's Name"
-                    name="oppenent"
-                    value={opponentOneName}
-                    onChange={(event) => setOpponentOneName(event.target.value)}
-                />
+                    <TextField sx={{ width: '25ch' , margin: 3, marginTop: 1 }}
+                        label="Location"
+                        type="text"
+                        required
+                        placeholder="Enter city or court name"
+                        name="location"
+                        value={courtlocation}
+                        onChange={(event) => setCourtlocation(event.target.value)}
+                    />
+                    <Button sx={{ margin: 4 }} color="primary" variant="contained" onClick={() => history.push('/dashboard')}>Back</Button>
+                    <Button input type="submit">Submit</Button>
 
-                <label>Second Opponent's Name</label>
-                <input
-                    type="text"
-                    placeholder="Enter Second Oppoenent's Name"
-                    name="oppenent-2"
-                    value={opponentTwoName}
-                    onChange={(event) => setOpponentTwoName(event.target.value)}
+                </form>
+            </div>
 
-                />
-                <br/>
-                <label>My Score</label>
-                <input
-                    type="text"
-                    required
-                    placeholder="Enter My Score"
-                    name="myScore"
-                    value={myScore}
-                    onChange={(event) => {
-                        const limit = 2;
-                        setMyscore(event.target.value.slice(0, limit));
-                    }}
-                />
-
-                <label>Opp Score</label>
-                <input
-                    type="text"
-                    required
-                    placeholder="Enter Opponents Score"
-                    name="oppScore"
-                    value={oppScore}
-                    onChange={(event) => {
-                        const limit = 2;
-                        setOppScore(event.target.value.slice(0, limit));
-                    }}
-                />
-                <br/>
-                <label>Location</label>
-                <input
-                    type="text"
-                    required
-                    placeholder="Enter city or court name"
-                    name="location"
-                    value={courtlocation}
-                    onChange={(event) => setCourtlocation(event.target.value)}
-                />
-                <input type="submit" />
-                <Button sx={{ margin: 4 }} color="primary" variant="contained" onClick={() => history.push('/dashboard')}>Back</Button>
-            </form>
-        </div>
-        
+        </Container>
     )
 }
 
